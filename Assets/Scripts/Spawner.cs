6afc1c93e2044031273cd505eab0ffc6a43acef6
hrallil <mathias.larsen0@gmail.com>
2022-03-10 12:25:58 +0100

    made walls with rotation, coners to be made

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,6 +8,8 @@ public class Spawner : MonoBehaviour
     public GameObject spikeTile;
     public GameObject enemyTile;
     public GameObject treasureTile;
+    public GameObject wallTile;
+    public GameObject cornerTile;
     public int roomSize = 3;
     
     // Start is called before the first frame update
@@ -15,8 +17,64 @@ public class Spawner : MonoBehaviour
         int tileSize = 5;
         for (var x = 0; x < roomSize; x++){
             for (var y = 0; y < roomSize; y++){
-                var position = new Vector3(x * tileSize, 0, y * tileSize);
-                Instantiate(pickTile(), position, Quaternion.identity);
+
+                /*
+                switch (x)
+                {
+                    case 0:
+                        var position = new Vector3(x * tileSize, 0, y * tileSize);
+                        Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 270));
+                    case roomSize - 1:
+                        var position = new Vector3(x * tileSize, 0, y * tileSize);
+                        Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 270));
+                    case 0:
+                        var position = new Vector3(x * tileSize, 0, y * tileSize);
+                        Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 270));
+                    case 0:
+                        var position = new Vector3(x * tileSize, 0, y * tileSize);
+                        Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 270));
+                                                                            
+                    default:
+                    rotation = Quaternion.identity;
+                }
+                */
+                if (x == 0){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 270));
+                } 
+                else if (y == 0){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 0));
+                } 
+                else if (y == roomSize - 1){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 180));
+                } 
+                else if (x == roomSize - 1){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 90));
+                } 
+                else if (x == roomSize - 1 && y == roomSize - 1){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(cornerTile, position, Quaternion.Euler(Vector3.down * 90));
+                } 
+                else if (x == roomSize - 1 && y == 0){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(cornerTile, position, Quaternion.Euler(Vector3.down * 90));
+                } 
+                else if (y == roomSize - 1 && x == 0){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(cornerTile, position, Quaternion.Euler(Vector3.down * 90));
+                } 
+                else if (x == 0 && y == 0){
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(cornerTile, position, Quaternion.Euler(Vector3.down * 90));
+                } 
+                else{
+                    var position = new Vector3(x * tileSize, 0, y * tileSize);
+                    Instantiate(pickTile(), position, Quaternion.identity);
+                }
+                
             }
         }
     }
