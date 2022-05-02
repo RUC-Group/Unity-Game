8679d9ad970918242ec223bb4cd7797a83fcc279
hrallil <mathias.larsen0@gmail.com>
2022-05-02 13:52:51 +0200

    Made 2d algorithm
    Self made random algorithm (SIMPLE)

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -17,19 +17,21 @@ public class Room : MonoBehaviour{
     public int posZ;
     public int roomSize;
 
-
     Vector2 globalPosition;
     
     public void setRoom(int posX, int posZ, int roomSize){
         this.posX=posX;
         this.posZ=posZ;
         this.roomSize=roomSize;
+
+        for (var i = 0; i < roomSize; i++){
+            for( var j = 0; j< roomSize; j++){
+                setTile(i,j);
+            }
+        }
+
     }
     
-
-    // Start is called before the first frame update
-    void Start(){
-    }
     public void setTile(int x, int z){
         roomTiles[x,z] = pickTile();
     }
@@ -54,14 +56,19 @@ public class Room : MonoBehaviour{
         globalPosition = position;
     }
 
+    public int indexToUnitePos(int i){
+        i = (5*5+15)*i;
+        return i;
+    }
+
     public async void showRoom(){
         for(int i = -1; i < roomSize+1; i++){
             for(int j = -1; j< roomSize+1; j++){
-                var position = new Vector3(i*5, 0, j*5 + posZ);
+                var position = new Vector3(i*5 + indexToUnitePos(posX), 0, j*5 + indexToUnitePos(posZ));
 
-                if(i == -1 && j==-1 || i==-1 && j==roomSize || i==roomSize && j == -1 || i==roomSize && j ==roomSize){
+                if(i == -1 && j == -1 || i == -1 && j == roomSize || i == roomSize && j == -1 || i == roomSize && j == roomSize){
                     Instantiate(cornerTile, position, Quaternion.identity);
-                }else if(i==-1 || i == roomSize){
+                }else if(i == -1 || i == roomSize){
                     Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 90));
                 }else if ( j== roomSize || j == -1){
                     Instantiate(wallTile, position, Quaternion.Euler(Vector3.down * 0));
