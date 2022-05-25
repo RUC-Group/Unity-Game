6c5f71e467fc847edf2b908e9f69458c05a97c3e
Dorian Sousa <calvo.dorian14@gmail.com>
2022-05-25 14:41:43 +0200

    Update Vertex.cs

--- Assets/Scripts/Vertex.cs
+++ Assets/Scripts/Vertex.cs
@@ -5,12 +5,11 @@ using UnityEngine;
 public class Vertex{
     public Vector3 pos {get;set;}
     public List<Edge> outEdges {get;set;}
-    bool visited = false;
+    
     //constructor
     public Vertex(Vector3 pos){
         this.pos = pos;
         outEdges = new List<Edge>();
-
     }
 
     public void addEdgeToList(Edge e){
