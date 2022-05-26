a050b95c2a3cdd4675ba4abcf57884a1f37097d4
Dorian Sousa <calvo.dorian14@gmail.com>
2022-05-26 21:28:23 +0200

    updated player

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -10,11 +10,9 @@ public class Player : MonoBehaviour{
     GameUI gameUI;
     GameObject model;
 
-    int speed = 8;
     int score = 0;
     int health = 100;
     int keyAmount = 0;
-    bool alive = true;
     float lastDamageTime = 0;
 
     // Start is called before the first frame update
@@ -27,33 +25,31 @@ public class Player : MonoBehaviour{
 
     // Update is called once per frame
     void Update(){
-        if(alive){
-            //float x = Input.GetAxis("Horizontal");
-            //float z = Input.GetAxis("Vertical");
+        if(health > 0){
             Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0 ,Input.GetAxisRaw("Vertical"));   
             Vector3 direction = input.normalized; 
-            Vector3 velocity = direction * speed;
+            Vector3 velocity = direction * 8;
             Vector3 position = velocity * Time.deltaTime;
+
             transform.Translate(position);
+
             if(position!=Vector3.zero){
                 model.transform.rotation = Quaternion.LookRotation(position) * Quaternion.Euler(0,270,0);
             }
+
             sword.updatePosition(model.transform,position);
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
               sword.attack();
             }
+
         }else{
             Quaternion target = Quaternion.Euler(0,90,90);
             transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime);
             health=0;
             SceneManager.LoadScene(2);
         }
-
-        if (health <= 0){
-            alive = !alive;
-        }
-
         gameUI.healthPoints=health;
         gameUI.score=score;
     }
@@ -102,10 +98,6 @@ public class Player : MonoBehaviour{
             health-=10;
             print(health);
             lastDamageTime = timeStamp;
-        }
-
-        if(health<=0){
-            alive=false;
         }
     }
 }
