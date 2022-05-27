97b0708681fd343cd7a1c214899e6c1481796bfc
hrallil <mathias.larsen0@gmail.com>
2022-05-27 23:28:38 +0200

    IDk what this, final push of the night

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour{
+    bool up = true;
+    int currTime = -5;
+    int rotate = 0;
+    // Start is called before the first frame update
+    void Start(){
+        rotate = UnityEngine.Random.Range(0,180); 
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(up){
+            currTime++;
+            transform.position = new Vector3(transform.position.x,transform.position.y + .03f,transform.position.z);
+        }
+        else{
+            currTime--;
+            transform.position = new Vector3(transform.position.x,transform.position.y - .03f,transform.position.z);
+        }
+        if(currTime == 20){
+            up = false;
+        }else if(currTime == -20){
+            up = true;
+        }
+        rotate++; 
+        transform.rotation = Quaternion.Euler(0,rotate,0);   
+    }
+}
