6820353c320e815883d7e13d9d0139cb78245e08
Dorian Sousa <calvo.dorian14@gmail.com>
2022-05-27 19:04:10 +0200

    Update Sword.cs

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -13,7 +13,7 @@ public class Sword : MonoBehaviour
     // Update is called once per frame
     void Update(){
         if(active){
-            if (yRotation<=135){
+            if (yRotation<=160){
                 timeCount = timeCount + Time.deltaTime*2;
                 yRotation += timeCount * speed;
                 transform.rotation=Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y-45+yRotation,transform.rotation.eulerAngles.z);
@@ -30,11 +30,11 @@ public class Sword : MonoBehaviour
     }
 
     public void updatePosition(Transform transform,Vector3 position){
-        this.transform.position=new Vector3((float)transform.position.x+(1*Mathf.Sin((this.transform.rotation.eulerAngles.y+45)*Mathf.Deg2Rad)),transform.position.y+1,transform.position.z+(1*Mathf.Cos((this.transform.rotation.eulerAngles.y+45)*Mathf.Deg2Rad)));
+        this.transform.position=new Vector3((float)transform.position.x+(1*Mathf.Sin((this.transform.rotation.eulerAngles.y+45)*Mathf.Deg2Rad)),transform.position.y,transform.position.z+(1*Mathf.Cos((this.transform.rotation.eulerAngles.y+45)*Mathf.Deg2Rad)));
         if(active){
-            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x+90,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z-60);
+            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x+90,transform.rotation.eulerAngles.y-20,transform.rotation.eulerAngles.z-60);
         }else{
-            this.transform.rotation = transform.rotation;
+            this.transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x,transform.rotation.eulerAngles.y,transform.rotation.eulerAngles.z+60);
         }
     }
 }
