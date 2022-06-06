350cc65101f86ee89c5d1948acaaa453d98a5973
Dorian Sousa <calvo.dorian14@gmail.com>
2022-06-06 10:43:32 +0200

    comment

--- Assets/Scripts/MainCamera.cs
+++ Assets/Scripts/MainCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MainCamera : MonoBehaviour{
     public Transform player;
 
-    // Update is called once per frame
+    // Update is called once per frame. Follows the player gameObject
     void Update(){
         player = GameObject.FindGameObjectWithTag("Player").transform;
         transform.position = player.transform.position + new Vector3((float)-0.53,(float) 18.38,(float) -20.45);
